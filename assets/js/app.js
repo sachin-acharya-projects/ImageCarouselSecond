ba56fd2e998d097825a762bda50fde797bb3d889
OWNER <acharyaraj71@gmail.com>
2021-03-14 17:39:24 +0545

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Hightligh');
+    var elems = document.querySelectorAll('.carousel');
+    options = {
+        
+    }
+    var instances = M.Carousel.init(elems, options);
+});
